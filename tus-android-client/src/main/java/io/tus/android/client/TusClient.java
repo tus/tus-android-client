b9fdb23d5e62d38d5f360ec8d55386e818fdb2f0
Acconut <maerious@gmail.com>
2015-06-29 21:54:03 +0200

    Remove unused imports in TusClient

--- tus-android-client/src/main/java/io/tus/android/client/TusClient.java
+++ tus-android-client/src/main/java/io/tus/android/client/TusClient.java
@@ -1,11 +1,7 @@
 package io.tus.android.client;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
