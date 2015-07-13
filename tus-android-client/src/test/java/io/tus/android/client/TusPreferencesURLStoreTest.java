b8744c3324e38346ac4fbff4f9cc7b527837bb03
Acconut <maerious@gmail.com>
2015-07-13 21:31:49 +0200

    Add test for TusPreferencesURLStore

---
+++ tus-android-client/src/test/java/io/tus/android/client/TusPreferencesURLStoreTest.java
@@ -0,0 +1,33 @@
+package io.tus.android.client;
+
+import android.app.Activity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+public class TusPreferencesURLStoreTest {
+
+    @Test
+    public void shouldSetGetAndDeleteURLs() throws Exception {
+        Activity activity = Robolectric.setupActivity(Activity.class);
+        TusPreferencesURLStore store = new TusPreferencesURLStore(activity.getSharedPreferences("tus-test", 0));
+        System.out.println("hello");
+        URL url = new URL("https://master.tus.io/files/hello");
+        String fingerprint = "foo";
+        store.set(fingerprint, url);
+
+        assertEquals(store.get(fingerprint), url);
+
+        store.remove(fingerprint);
+
+        assertEquals(store.get(fingerprint), null);
+    }
+}
