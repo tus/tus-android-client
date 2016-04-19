71fbdfce241b82ef90c2764ed86137bf1a5666d8
Marius <maerious@gmail.com>
2016-04-19 09:37:15 +0200

    Put filename by default into metadata

--- tus-android-client/src/main/java/io/tus/android/client/TusAndroidUpload.java
+++ tus-android-client/src/main/java/io/tus/android/client/TusAndroidUpload.java
@@ -6,20 +6,32 @@ import android.net.Uri;
 import android.provider.OpenableColumns;
 
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
 import io.tus.java.client.TusUpload;
 
 public class TusAndroidUpload extends TusUpload {
     public TusAndroidUpload(Uri uri, Activity activity) throws FileNotFoundException {
-        Cursor cursor = activity.getContentResolver().query(uri, new String[]{OpenableColumns.SIZE}, null, null, null);
+        Cursor cursor = activity.getContentResolver().query(uri, new String[]{OpenableColumns.SIZE, OpenableColumns.DISPLAY_NAME}, null, null, null);
+        if(cursor == null) {
+            throw new FileNotFoundException();
+        }
+
         int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
+        int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
         cursor.moveToFirst();
         long size = cursor.getLong(sizeIndex);
+        String name = cursor.getString(nameIndex);
 
         setSize(size);
         setInputStream(activity.getContentResolver().openInputStream(uri));
 
         setFingerprint(String.format("%s-%d", uri.toString(), size));
+
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("filename", name);
+        setMetadata(metadata);
 
         cursor.close();
     }
