667ff2c7af96497f8b12ae82f2850e0daa735523
Acconut <maerious@gmail.com>
2018-11-10 23:23:31 +0000

    Use ParcelFileDescriptor for resolving filesize

--- tus-android-client/src/main/java/io/tus/android/client/TusAndroidUpload.java
+++ tus-android-client/src/main/java/io/tus/android/client/TusAndroidUpload.java
@@ -1,11 +1,15 @@
 package io.tus.android.client;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.ParcelFileDescriptor;
 import android.provider.OpenableColumns;
+import android.util.Log;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,19 +17,36 @@ import io.tus.java.client.TusUpload;
 
 public class TusAndroidUpload extends TusUpload {
     public TusAndroidUpload(Uri uri, Activity activity) throws FileNotFoundException {
-        Cursor cursor = activity.getContentResolver().query(uri, new String[]{OpenableColumns.SIZE, OpenableColumns.DISPLAY_NAME}, null, null, null);
+        ContentResolver resolver = activity.getContentResolver();
+        Cursor cursor = resolver.query(uri, new String[]{OpenableColumns.SIZE, OpenableColumns.DISPLAY_NAME}, null, null, null);
         if(cursor == null) {
             throw new FileNotFoundException();
         }
 
-        int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
         int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
         cursor.moveToFirst();
-        long size = cursor.getLong(sizeIndex);
         String name = cursor.getString(nameIndex);
 
+        // On some files, ContentResolver#query will report the wrong filesize
+        // even though the InputStream reads the correct length. This discrepancy
+        // causes mismatching upload offsets when the upload should be finished.
+        // Using the stat size from a file descriptor seems to always report the
+        // correct filesize.
+        // See https://github.com/tus/tus-android-client/issues/25
+        // See https://stackoverflow.com/questions/21882322/how-to-correctly-get-the-file-size-of-an-android-net-uri
+        ParcelFileDescriptor fd = resolver.openFileDescriptor(uri, "r");
+        if(fd == null) {
+            throw new FileNotFoundException();
+        }
+        long size = fd.getStatSize();
+        try {
+            fd.close();
+        } catch (IOException e) {
+            Log.e("TusAndroidUpload", "unable to close ParcelFileDescriptor", e);
+        }
+
         setSize(size);
-        setInputStream(activity.getContentResolver().openInputStream(uri));
+        setInputStream(resolver.openInputStream(uri));
 
         setFingerprint(String.format("%s-%d", uri.toString(), size));
 
